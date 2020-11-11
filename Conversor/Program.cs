8e52f85c7a243cc10bda338d6bf288d32cf830fa
Lucas Denipoti <lucasfdenipoti@gmail.com>
2020-11-11 16:44:44 -0300

    program feito

--- Conversor/Program.cs
+++ Conversor/Program.cs
@@ -6,7 +6,15 @@ namespace Conversor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Qual é Cotação do Dólar? "); // Entrada do usuário para a cotação do dólar
+            Conversor.cotacao = double.Parse(Console.ReadLine());
+
+            Console.Write("Quantos Dólares Você Vai Comprar? "); // Entrada do usuário para a quantidade em dólar
+            Conversor.valorCompra = double.Parse(Console.ReadLine());
+         
+            Console.Write("Valor a Ser Pago em Reais: " + Conversor.ValorReais().ToString("F2", CultureInfo.InvariantCulture)); // Escrever valor em reais
+
+            Console.ReadKey();
         }
     }
 }
